f44e348449b32ce596022a90978e189588cdcd7e
regalonefour <95228586+regalonefour@users.noreply.github.com>
2022-03-26 03:54:47 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,6 +1,8 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 local debugProps, sitting, lastPos, currentSitCoords, currentScenario, occupied = {}
+local curobjpos = nil
+local playerPos = nil
 local disableControls = false
 local currentObj = nil
 
@@ -18,25 +20,50 @@ local function hideNUI()
     Wait(0)
 end
 
+local function DrawTxt(x, y, width, height, scale, text, r, g, b, a, outline)
+    SetTextFont(4)
+    SetTextProportional(0)
+    SetTextScale(scale, scale)
+    SetTextColour(r, g, b, a)
+    SetTextDropShadow(0, 0, 0, 0,255)
+    SetTextEdge(2, 0, 0, 0, 255)
+    SetTextDropShadow()
+    SetTextOutline()
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(x - width/2, y - height/2 + 0.005)
+end
+
+
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
 		local playerPed = PlayerPedId()
 
-		if sitting then
-			displayNUIText() 
-		else
-			hideNUI() 
+		if sitting and not IsPedUsingScenario(playerPed, currentScenario) then
+			TaskStartScenarioAtPosition(playerPed, currentScenario, curobjpos.x, curobjpos.y, curobjpos.z, GetEntityHeading(playerPed), 0, true, false)			
+			--wakeup()
 		end
 
-		if sitting and not IsPedUsingScenario(playerPed, currentScenario) then
+		if sitting and isCurDead then
 			wakeup()
 		end
 
-		if IsControlPressed(0, 23) and IsInputDisabled(0) and IsPedOnFoot(playerPed) then
+		if IsControlPressed(0, 23) then
 			if sitting then
 				wakeup()
 			end			
+		end
+	end
+end)
+
+CreateThread(function()
+	while true do
+		Wait(1000)
+		if QBCore.Functions.GetPlayerData().metadata["isdead"] or QBCore.Functions.GetPlayerData().metadata["inlaststand"] then
+			isCurDead = true
+		else
+			isCurDead = false
 		end
 	end
 end)
@@ -48,19 +75,41 @@ Citizen.CreateThread(function()
 		local model = GetHashKey(v)
 		table.insert(Sitables, model)
 	end
+
 	Citizen.Wait(100)
 	exports['qb-target']:AddTargetModel(Sitables, {
         options = {
             {
                 event = "qb-Sit:Sit",
                 icon = "fas fa-chair",
-                label = "Use",
+                label = "Sit Down",
 				entity = entity
             },
         },
         job = {"all"},
         distance = Config.MaxDistance
     })
+	for k, v in pairs(Config.SitPoints) do
+		exports['qb-target']:AddBoxZone("Sitpoint"..k, vector3(v.coords.x, v.coords.y, v.coords.z), 0.75, 0.75, {
+			name = "Sitpoint"..k,
+			heading = v.coords.w,
+			debugPoly = false,
+			minZ = v.coords.z - 0.5,
+			maxZ = v.coords.z + 0.5,
+		}, {
+		options = {
+			{
+				type = "client",
+				event = "qb-Sit:SitPoint",
+				icon = "fas fa-chair",
+				label = "Sit Down",
+				sitpoint = k,
+				coords = v.coords
+			},
+		},
+		distance = 2.5
+		})
+	end
 end)
 
 RegisterNetEvent("qb-Sit:Sit", function(data)
@@ -74,24 +123,36 @@ RegisterNetEvent("qb-Sit:Sit", function(data)
 		DisableControlAction(1, 37, true)
 	end
 
-	local object, distance = data.entity, #(GetEntityCoords(playerPed) - GetEntityCoords(data.entity))
+	local object, hash = data.entity, GetEntityModel(data.entity)
 
-	if distance and distance < 1.4 then
-		local hash = GetEntityModel(object)
-
-		for k,v in pairs(Config.Sitable) do
-			if GetHashKey(k) == hash then
-				sit(object, k, v)
-				break
-			end
+	for k,v in pairs(Config.Sitable) do
+		if GetHashKey(k) == hash then
+			sit(object, k, v)
+			break
 		end
 	end
+end)
+
+RegisterNetEvent("qb-Sit:SitPoint", function(data)
+	local playerPed = PlayerPedId()
+
+	if sitting and not IsPedUsingScenario(playerPed, currentScenario) then
+		wakeup()
+	end
+
+	if disableControls then
+		DisableControlAction(1, 37, true)
+	end
+
+	sitpoint(data.sitpoint, data.coords)
 end)
 
 
 function wakeup()
 	local playerPed = PlayerPedId()
 	local pos = GetEntityCoords(PlayerPedId())
+	
+	hideNUI()
 
 	TaskStartScenarioAtPosition(playerPed, currentScenario, 0.0, 0.0, 0.0, 180.0, 2, true, false)
 	while IsPedUsingScenario(PlayerPedId(), currentScenario) do
@@ -100,7 +161,6 @@ function wakeup()
 	ClearPedTasks(playerPed)
 
 	FreezeEntityPosition(playerPed, false)
-	FreezeEntityPosition(currentObj, false)
 	TriggerServerEvent('qb-sit:leavePlace', currentSitCoords)
 	currentSitCoords, currentScenario = nil, nil
 	sitting = false
@@ -111,14 +171,17 @@ function sit(object, modelName, data)
 	if not HasEntityClearLosToEntity(PlayerPedId(), object, 17) then
 		return
 	end
+	
 	disableControls = true
 	currentObj = object
 	FreezeEntityPosition(object, true)
 
-	PlaceObjectOnGroundProperly(object)
-	local pos = GetEntityCoords(object)
-	local playerPos = GetEntityCoords(PlayerPedId())
-	local objectCoords = pos.x .. pos.y .. pos.z
+	playerPos = GetEntityCoords(PlayerPedId())
+	curobjpos = GetEntityCoords(object)
+	curobjpos = vector3(curobjpos.x, curobjpos.y, curobjpos.z + (playerPos.z - curobjpos.z)/2)
+	local objectCoords = curobjpos
+
+	print(objectCoords)
 
 	QBCore.Functions.TriggerCallback('qb-sit:getPlace', function(occupied)
 		if occupied then
@@ -127,15 +190,46 @@ function sit(object, modelName, data)
 			local playerPed = PlayerPedId()
 			lastPos, currentSitCoords = GetEntityCoords(playerPed), objectCoords
 
+			displayNUIText()
 			TriggerServerEvent('qb-sit:takePlace', objectCoords)
 			
 			currentScenario = data.scenario
-			TaskStartScenarioAtPosition(playerPed, currentScenario, pos.x, pos.y, pos.z + (playerPos.z - pos.z)/2, GetEntityHeading(object) + 180.0, 0, true, false)
+			TaskStartScenarioAtPosition(playerPed, currentScenario, curobjpos.x, curobjpos.y, curobjpos.z - data.verticalOffset, GetEntityHeading(object) + 180.0, 0, true, false)
 
 			Citizen.Wait(2500)
 			if GetEntitySpeed(PlayerPedId()) > 0 then
 				ClearPedTasks(PlayerPedId())
-				TaskStartScenarioAtPosition(playerPed, currentScenario, pos.x, pos.y, pos.z + (playerPos.z - pos.z)/2, GetEntityHeading(object) + 180.0, 0, true, true)
+				TaskStartScenarioAtPosition(playerPed, currentScenario, curobjpos.x, curobjpos.y, curobjpos.z - data.verticalOffset, GetEntityHeading(object) + 180.0, 0, true, true)
+			end
+
+			sitting = true
+		end
+	end, objectCoords)
+end
+
+function sitpoint(sitpoint, coords)
+	disableControls = true
+
+	playerPos = GetEntityCoords(PlayerPedId())
+	curobjpos = coords
+	local objectCoords = coords.x .. coords.y .. coords.z
+
+	QBCore.Functions.TriggerCallback('qb-sit:getPlace', function(occupied)
+		if occupied then
+			QBCore.Functions.Notify('Someone is already sitting here.', 'error')
+		else
+			local playerPed = PlayerPedId()
+			lastPos, currentSitCoords = GetEntityCoords(playerPed), objectCoords
+
+			TriggerServerEvent('qb-sit:takePlace', objectCoords)
+			
+			currentScenario = 'PROP_HUMAN_SEAT_BENCH'
+			TaskStartScenarioAtPosition(playerPed, currentScenario, coords.x, coords.y, coords.z, coords.w, 0, true, false)
+
+			Citizen.Wait(2500)
+			if GetEntitySpeed(PlayerPedId()) > 0 then
+				ClearPedTasks(PlayerPedId())
+				TaskStartScenarioAtPosition(playerPed, currentScenario, coords.x, coords.y, coords.z, coords.w, 0, true, true)
 			end
 
 			sitting = true
