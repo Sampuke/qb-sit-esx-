b485740169f58e4673a7251503520746b5ed0f7d
regalonefour <95228586+regalonefour@users.noreply.github.com>
2022-04-17 19:42:44 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -20,21 +20,6 @@ local function hideNUI()
     Wait(0)
 end
 
-local function DrawTxt(x, y, width, height, scale, text, r, g, b, a, outline)
-    SetTextFont(4)
-    SetTextProportional(0)
-    SetTextScale(scale, scale)
-    SetTextColour(r, g, b, a)
-    SetTextDropShadow(0, 0, 0, 0,255)
-    SetTextEdge(2, 0, 0, 0, 255)
-    SetTextDropShadow()
-    SetTextOutline()
-    SetTextEntry("STRING")
-    AddTextComponentString(text)
-    DrawText(x - width/2, y - height/2 + 0.005)
-end
-
-
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
